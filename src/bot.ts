da4dba6c8f312ae823ef13e6b2c5e77374495b53
isaias-silva <zackblackmangold@gmail.com>
2022-07-20 21:51:19 -0300

    definindo conexão e monitoramento de mensagens

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,14 @@
+//conexão
 import { connect } from "./connection";
 
-export function bot(){
-    const socket= connect()
+//exportando a inicialização do bot
+export async function bot() {
+    //conectando
+    const socket = await connect()
+    //socket monitorando evento de messagem recebida
+    socket.ev.on('messages.upsert', async (msg) => {
+        const [wMessage] = msg.messages
+        console.log(`messagem recebida: ${wMessage.message?.conversation}`)
+
+    })
 }
--- src/connection.ts
+++ src/connection.ts
@@ -1,35 +1,36 @@
-import makeWaSocket, {
-    DisconnectReason,
-    useSingleFileAuthState,
-  } from "@adiwajshing/baileys";
-  import { Boom } from "@hapi/boom";
-  import path from "path";
-  
-  export const connect = async () => {
-   
-    const { state, saveState } = useSingleFileAuthState(
-      path.resolve(__dirname, "..", "cache", "auth.json")
-    );
-  
-    const socket = makeWaSocket({
-      printQRInTerminal: true,
-      auth: state,
-    });
-  
-    socket.ev.on("connection.update", async (update) => {
-      const { connection, lastDisconnect } = update;
-      if (connection === "close") {
-        const shouldReconnect =
-          (lastDisconnect?.error as Boom)?.output?.statusCode !==
-          DisconnectReason.loggedOut;
-  
-        if (shouldReconnect) {
-          await connect();
-        }
+import makeWaSocket,{
+  DisconnectReason,
+  useSingleFileAuthState,
+} from "@adiwajshing/baileys";
+
+
+import { Boom } from "@hapi/boom";
+import path from "path";
+
+export const connect =async () => {
+  const { state, saveState } = useSingleFileAuthState(
+    path.resolve(__dirname, "..", "cache", "auth_info_multi.json")
+  );
+
+  const socket = makeWaSocket({
+    printQRInTerminal: true,
+    auth: state,
+  });
+ 
+  socket.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      const shouldReconnect =
+        (lastDisconnect?.error as Boom)?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+
+      if (shouldReconnect) {
+        await connect();
       }
-    });
-  
-    socket.ev.on("creds.update", saveState);
-  
-    return socket;
-  };
+    }
+  });
+
+  socket.ev.on("creds.update", saveState);
+
+  return socket;
+};
