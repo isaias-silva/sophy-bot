50f0dd9866e963f15fc61c6949f6cb3a92bb9cce
isaias-silva <zackblackmangold@gmail.com>
2022-07-22 20:39:00 -0300

    images

--- src/functions/botFunction.ts
+++ src/functions/botFunction.ts
@@ -1,5 +1,6 @@
 import { proto } from "@adiwajshing/baileys";
 import { Ibot } from "../interfaces/Ibot";
+import fs from 'fs'
 //exportando as funções do bot
 export const getBotfunctions = (socket: any, webMessage: proto.IWebMessageInfo) => {
     //id do chat onde foi enviada a messangem
@@ -13,14 +14,23 @@ export const getBotfunctions = (socket: any, webMessage: proto.IWebMessageInfo) 
         return socket.sendMessage(remoteJid, { text: txt }, { quoted: webMessage })
     }
     //marcar usuario
-    const mark: Ibot["mark"] = async (txt: string,id:string ) => {
-        return socket.sendMessage(remoteJid,{ text: `@${id.split(`@`)[0]}, ${txt}`, mentions: [id] })
+    const mark: Ibot["mark"] = async (txt: string, id: string) => {
+        return socket.sendMessage(remoteJid, { text: `@${id.split(`@`)[0]}, ${txt}`, mentions: [id] })
     }
-
+    const sendImage: Ibot["sendImage"] = async (pathOrBuffer: Buffer | string, caption?: string, isReply?: boolean) => {
+        const image = pathOrBuffer instanceof Buffer ? pathOrBuffer : fs.readFileSync(pathOrBuffer);
+        const params = {
+            image,
+            caption: caption
+        }
+        let options = isReply == true ? { quoted: webMessage } : {}
+        return socket.sendMessage(remoteJid, params, options)
+    }
     return {
         sendText,
         reply,
         mark,
+        sendImage,
         remoteJid,
 
     }
