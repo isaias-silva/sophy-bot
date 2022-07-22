2044436fa7780c9f23770ed9d8c3e0e80042c158
isaias-silva <zackblackmangold@gmail.com>
2022-07-22 15:42:04 -0300

    comentarios

--- src/functions/botFunction.ts
+++ src/functions/botFunction.ts
@@ -1,13 +1,18 @@
 import { proto } from "@adiwajshing/baileys";
 import { Ibot } from "../interfaces/Ibot";
+//exportando as funções do bot
 export const getBotfunctions = (socket: any, webMessage: proto.IWebMessageInfo) => {
+    //id do chat onde foi enviada a messangem
     const { remoteJid } = webMessage.key
+    //enviar somente texto
     const sendText: Ibot["sendText"] = async (txt: string) => {
         return socket.sendMessage(remoteJid, { text: txt })
     }
+    //responder mensagem
     const reply: Ibot["reply"] = async (txt: string) => {
         return socket.sendMessage(remoteJid, { text: txt }, { quoted: webMessage })
     }
+    //marcar usuario
     const mark: Ibot["mark"] = async (txt: string,id:string ) => {
         return socket.sendMessage(remoteJid,{ text: `@${id.split(`@`)[0]}, ${txt}`, mentions: [id] })
     }
