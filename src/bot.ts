97fa2f9c46b9c9d828d53c021fcc427db6b2840b
isaias-silva <zackblackmangold@gmail.com>
2022-07-28 20:28:40 -0300

    logica dos comandos

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,5 @@
 //conexão
+import { data } from "./config/data";
 import { connect } from "./connection";
 import { getBotfunctions } from "./functions/botFunction";
 import { isComand, searchComand } from "./functions/comand";
@@ -10,19 +11,25 @@ export async function bot() {
     //socket monitorando evento de messagem recebida
     socket.ev.on('messages.upsert', async (msg) => {
         const [wMessage] = msg.messages
-     
-        const message = wMessage.message
-       const botF= getBotfunctions(socket, wMessage)
 
+        const message = wMessage.message
+        const botF = getBotfunctions(socket, wMessage)
+        //barreiras
+
+        //se message nao existe
         if (!message) {
             return
         }
-      
+        //se message nao tem o prefixo
         if (!isComand(message)) {
             return
 
         }
-       
+        //se o comando nao existe
+        if (!searchComand(wMessage)) {
+            return botF.reply(`comando não encontrado! para ver os comandos digite *${data.prefix}menu*`)
+        }
 
+        //sem barreiras, comandos seguem apartir daqui
     })
 }
--- src/functions/comand.ts
+++ src/functions/comand.ts
@@ -1,19 +1,28 @@
 import { proto } from "@adiwajshing/baileys"
 import { data } from "../config/data"
 import { Ibot } from "../interfaces/Ibot"
-
+import fs from 'fs'
+import path from 'path'
+import comandsList from "../config/comandsList"
 export function isComand(message: proto.IMessage) {
-   
+
     const texto = message?.conversation || message?.imageMessage?.caption || message?.extendedTextMessage?.text
     if (!texto) { return }
-    console.log(texto)
+
     let prefix = texto.split("")[0]
     if (prefix == data.prefix) {
         return true
     } else return false
 }
-export function searchComand(message: proto.IMessage) {
-    console.log(message)
+export function searchComand(Webmessage: proto.IWebMessageInfo) {
+    const { message } = Webmessage
 
-
+    const texto = message?.conversation || message?.imageMessage?.caption || message?.extendedTextMessage?.text
+    const comand = texto?.replace(data.prefix, "")
+    let exists = comandsList.find(str => str == comand)
+    if (exists) {
+        return true
+    } else {
+        return false
+    }
 }
