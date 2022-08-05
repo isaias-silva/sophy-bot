ec589084ed6a20e69a126f4119c68010e624241a
isaias-silva <zackblackmangold@gmail.com>
2022-08-04 22:05:31 -0300

    finalizando ban

--- src/comands/ban.ts
+++ src/comands/ban.ts
@@ -1,44 +1,53 @@
 import { Ibot } from "../interfaces/Ibot";
 
 export async function ban(bot: Ibot, phoneid?: string) {
-    const { socket, isGroup, reply, remoteJid, webMessage, isAdmin,isSuperAdmin,imAdmin} = bot
+    const { socket, isGroup, isReply, reply, remoteJid, webMessage, isAdmin, isSuperAdmin, imAdmin } = bot
     const { participant } = webMessage.key
-  if(!participant){
-    return
-  }
+    const phonemark = webMessage.message?.extendedTextMessage?.contextInfo?.participant
+
+    if (!participant) {
+        return
+    }
 
     if (!isGroup) {
         return reply(`comando só usável em grupos!`)
     }
 
-    if (!phoneid) {
-        return reply('marque o numero para banir')
-    }
-    
-    let number = `${phoneid.replace('@', '')}@s.whatsapp.net`
- 
-    let admin= await isAdmin(participant)
-    let superadmin= await isSuperAdmin(number)
-    let botadmin= await imAdmin()
 
-    if(!botadmin){
+    let number = isReply ? phonemark : `${phoneid?.replace('@', '')}@s.whatsapp.net`
+    if (!number) {
+        return
+    }
+    let admin = await isAdmin(participant)
+    let superadmin = await isSuperAdmin(number)
+    let botadmin = await imAdmin()
+
+    if (!botadmin) {
         return reply('preciso ser adm para fazer isso!')
     }
 
-    if(!admin){
+    if (!admin) {
         return reply('apenas adms podem usar esse comando!')
     }
 
-    if(superadmin){
+    if (superadmin) {
         return reply('nao se pode remover o criador do grupo!')
     }
 
     try {
-        await socket.groupParticipantsUpdate(
-            remoteJid,
-            [number],
-            "remove"
-        )
+        await reply('removendo...')
+        setTimeout(async () => {
+
+            await socket.groupParticipantsUpdate(
+                remoteJid,
+                [number],
+                "remove"
+            )
+        }, 3000);
+        return setTimeout(async () => {
+            await reply('alvo removido com sucesso! ⚔️')
+
+        }, 4000)
     } catch (err) {
         console.log(err)
         return reply('erro ao banir')
