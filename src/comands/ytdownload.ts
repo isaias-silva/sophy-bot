6e6cf2cec53cebb61a4b90dd07e5d2d7acdd6423
isaias-silva <zackblackmangold@gmail.com>
2022-08-08 21:27:38 -0300

    yt finalizado

--- src/comands/ytdownload.ts
+++ src/comands/ytdownload.ts
@@ -1,7 +1,19 @@
+import { data } from "../config/data";
 import { downloadYtVideo } from "../functions/youtubeFunctions";
 import { Ibot } from "../interfaces/Ibot";
+import fs from 'fs'
+export async function ytdownload(bot: Ibot, link: string) {
+   const { reply, sendVideo } = bot
+   if (!link) {
+      return reply(`envie o comando com o link\n ex: *${data.prefix}ytdownload http://youtube/exemplo*`)
+   }
+   const file = await downloadYtVideo(link)
+   if (!file) {
+      return reply('erro ao baixar o video, envie um link válido e que nao ultrapasse *10 minutos*!')
+   }
+   const { path, videocaption } = file
+   await reply('um minutinho...isso pode demorar')
+   await sendVideo(path, videocaption, true)
+   return fs.unlinkSync(path)
 
-export async function ytdownload(bot:Ibot,link:string){
-   let file= await downloadYtVideo(link)
-   console.log(file)
 }
--- src/functions/youtubeFunctions.ts
+++ src/functions/youtubeFunctions.ts
@@ -5,7 +5,8 @@ import { randomtitle } from "./random";
 export const downloadYtVideo = async function(link:string) {
    let title= randomtitle()
    let videoinfo= await ytdl.getInfo(link)
-   const limit=420
+   let videocaption=videoinfo.videoDetails.title
+   const limit=660
    const duration= parseInt(videoinfo.videoDetails.lengthSeconds)
    if(duration>limit){
       return null
@@ -17,5 +18,5 @@ export const downloadYtVideo = async function(link:string) {
         buffer = Buffer.concat([buffer, chunk])
     }
      fs.writeFileSync(path.resolve("assets","temp",`${title}.mp4`),buffer)
-     return `${title}.mp4`
+     return {path:path.resolve("assets","temp",`${title}.mp4`),videocaption}
 }
