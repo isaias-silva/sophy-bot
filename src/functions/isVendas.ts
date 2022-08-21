6bc5d83bcdc81fe5bbac912dc0dd00d3c4142113
isaias-silva <zackblackmangold@gmail.com>
2022-08-21 15:20:42 -0300

    antivendas implemented

--- src/functions/isVendas.ts
+++ src/functions/isVendas.ts
@@ -6,7 +6,24 @@ export default function (message: proto.IMessage) {
         return false
     }
 
-    const forbiddenwors = ["vendo", "pack", "seguidores", "pix", "notas fake", "nota fake"]
+    const forbiddenwors = [
+        "vendo",
+        "pack",
+        "seguidores",
+        "pix",
+        "notas fake",
+        "nota fake",
+        "R$",
+        "$",
+        "SANTANDER",
+        "ITAU",
+        "LOJAS ITAÚ",
+        "HIPER CARD",
+        "CREDI CARD",
+        "NUBANK",
+        "BRADESCO",
+        "NEXT",
+        "BANCO DO BRASIL"]
     for (let word of forbiddenwors) {
         if (frase.includes(word.toLowerCase()) || frase.includes(word.toUpperCase()) || frase.includes(word[0].toUpperCase() + word.substring(1))) {
             return true
